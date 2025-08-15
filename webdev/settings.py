f1934d8c3a88d2406ec7fa572b0b8508b176f512
GBTorto <moraisgabriel867@gmail.com>
2025-08-15 14:32:00 -0300

    debug 5.0

--- webdev/settings.py
+++ webdev/settings.py
@@ -121,11 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-STATICFILES_DIRS = [
-    BASE_DIR / "mycontacts/static",
-]
+STATIC_ROOT = "/home/GBTorto/django-pro/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
