d54ba5a0412d03e99d8ae6f7ef0b8ac006ce3176
GBTorto <moraisgabriel867@gmail.com>
2025-08-15 14:22:26 -0300

    debug 3.0

--- webdev/settings.py
+++ webdev/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/GBTorto/django-pro/mycontacts/static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "mycontacts/static",
