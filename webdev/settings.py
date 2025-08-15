4adb461422368aa53857c29d3574260dcc762dde
GBTorto <moraisgabriel867@gmail.com>
2025-08-15 14:36:16 -0300

    debug 6.0

--- webdev/settings.py
+++ webdev/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/home/GBTorto/django-pro/static"
+STATIC_ROOT = "/home/GBTorto/django-pro/mycontacts/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
